94df4991b3b89d4e025616049ca405dc4d541ce3
phlafoo <shuval@telus.net>
2024-12-10 01:30:07 -0700

    day 9 complete

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -62,19 +62,19 @@ pub fn process(input: &str) -> miette::Result<String> {
     let mut left_id = 0;
     let mut right_id = len / 2;
 
-    let mut i = 0; // Left index
-    let mut j = len - 1; // Right index
+    let mut left = 0; // Left index
+    let mut right = len - 1; // Right index
 
     let mut result = 0;
     let mut pos = 0;
-    let mut remaining = parse_u8(input[j]); // How many ids are remaining from the right side
+    let mut remaining = parse_u8(input[right]); // How many ids are remaining from the right side
 
     let calculate_step = |id, count, pos| -> usize {
         (id * count * (count + 2 * pos - 1)) / 2
     };
 
     'outer: loop {
-        if j <= i {
+        if right <= left {
             // Left and right pointers have met
             // Add last remaining ids
             result += calculate_step(left_id, remaining, pos);
@@ -82,18 +82,18 @@ pub fn process(input: &str) -> miette::Result<String> {
         }
 
         // Process file block from the left
-        let in_left = parse_u8(input[i]);
+        let in_left = parse_u8(input[left]);
         result += calculate_step(left_id, in_left, pos);
 
         // Process empty block from the left
         pos += in_left;
-        i += 1;
+        left += 1;
         left_id += 1;
-        let mut remaining_empty = parse_u8(input[i]);
+        let mut remaining_empty = parse_u8(input[left]);
 
         // Loop until the empty block is filled with files from the right
         loop {
-            if j <= i {
+            if right <= left {
                 // Left and right pointers have met
                 break 'outer;
             }
@@ -105,15 +105,15 @@ pub fn process(input: &str) -> miette::Result<String> {
 
                 // Empty block is filled, move to next file from the left
                 pos += count;
-                i += 1;
+                left += 1;
 
                 break;
             } else {
                 let count = remaining;
                 remaining_empty -= count;
 
-                j -= 2;
-                remaining = parse_u8(input[j]);
+                right -= 2;
+                remaining = parse_u8(input[right]);
 
                 result += calculate_step(right_id, count, pos);
                 right_id -= 1;
@@ -128,6 +128,7 @@ pub fn process(input: &str) -> miette::Result<String> {
 }
 // 6353658451014
 
+#[inline(always)]
 fn parse_u8(b: u8) -> usize {
     (b - b'0') as usize
 }
@@ -140,34 +141,19 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        // let input = "2333133121414131402";
-        // let a = process(input)?;
-        // let b = process1(input)?;
+        let input = "2333133121414131402";
+        assert_eq!("1928", process(input)?);
 
-        // if let Some(e) = a.iter().zip(b.clone()).find(|(a, b)| *a != b) {
-        //     panic!("{}, {}", e.0, e.1);
+        // for _ in 0..10 {
+        //     let input = get_input();
+        //     let input = input.as_str();
+
+        //     assert_eq!(process(input)?, process_naive(input)?, "INPUT: {input}");
         // }
-        // assert_eq!(a, b, "{:?}\n\n{:?}", a, b);
-
-        // assert_eq!("1928", process(input)?);
-        // let input = "1121301";
-
-        for _ in 0..10 {
-            let input = get_input();
-            let input = input.as_str();
-
-            assert_eq!(process(input)?, process_naive(input)?, "INPUT: {input}");
-        }
-
-        // let input = "9130516303707930824472047296569685782634907323869243854531871452348163127494304435158484071376708528315121461192641810632762750185821910445209640158493646506120563197694112986690223491389790263030750399905648970961917332387918733117427991813918861583395484440898572277343127329023363732806309434171442152343592048740237053917849822401059368321448690431276394345821822474499775702551718447557341997839133803177680389616039622959676099330179012705711641214954207032765968113344505659230905656769298872696856221072606236591898058985263612203979167131077927208172036583850560936175069157596937426783184510479179530675719930511418692976494368716234438973550539719215319416548773587049683720636619915525366979301131378829295909510334135787117675806983408265686816554084763265070989877788904798613652634907933628092165225202269487981155142518962532771229443939192905711828630795837235853245292763048074746710333379769960852757130715065686342856839454494457556128963418298478620875370328287617630318133118681205893815610648901196585034682916343588623361212063434654487546904907295046544848570333371820288492147817070837550932747929486251180878791035186597395387276849990867221760339714999747922821669207324618642679212476767996518450303223110947552966523273859367724879581195996091048091032741963735455663258415699996478575925102028381061866127996618046023880199244028736494870842724441491783020783145462596328582598833891227922652124636945793690884661095230664029913188310728226298775531949686187206728585157407872126981393049022946920871987769314871863526629815337644907431951737794606969610367537492214067735530474448066051066828705056287721492992426181723652593726773664076748387169771994021440742536165493269110309";
-        // left: "3856138847"
-        //right: "3856138848"
-        // 022111222......
-        // 2*1 + 2*2 + 1*3 + 1*4 + 1*5 + 2*6 + 2*7 + 2*8
-        // assert_eq!("3856138848", process(input)?);
         Ok(())
     }
 
+    #[allow(unused)]
     fn get_input() -> String {
         let mut rng = thread_rng();
 
--- day-09/src/part2.rs
+++ day-09/src/part2.rs
@@ -1,6 +1,88 @@
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    todo!("part 2");
+    let input = input.as_bytes();
+
+    // True if the file has been moved
+    let mut moved = vec![false; input.len()];
+
+    let mut left_id = 0;
+    let mut pos = 0; // Index of the rearranged list of IDs
+    let mut left = 0; // Left index
+
+    let calculate_step = |id, count, pos| -> usize {
+        (id * count * (count + 2 * pos - 1)) / 2
+    };
+    
+    // Indexed by file size. Indicates where in the input the leftmost file was for a given size.
+    let mut cache = [input.len() - 1; 10];
+    let update_cache = |cache: &mut [usize; 10], i, val| {
+        for c in cache.iter_mut().take(i + 1) {
+            *c = (*c).min(val);
+        }
+    };
+    let mut result = 0;
+
+    while left != input.len() - 1 {
+        let left_size = parse_u8(input[left]);
+
+        // Add to result if this file hasn't been moved
+        if !moved[left] {
+            result += calculate_step(left_id, left_size, pos);
+        }
+        left_id += 1;
+        left += 1;
+        pos += left_size;
+
+        let mut hole = parse_u8(input[left]);
+        let mut right = cache[hole]; // Start at leftmost position of last known same sized file
+        let mut right_id = right / 2 + 1;
+
+        // Fill hole
+        while right > left {
+            right_id -= 1;
+
+            // Skip if moved already
+            if moved[right] {
+                right -= 2;
+                continue;
+            }
+            // Get size of file
+            let right_size = parse_u8(input[right]);
+
+            // If it fits it sits
+            if right_size <= hole {
+                update_cache(&mut cache, hole, right);
+                
+                result += calculate_step(right_id, right_size, pos);
+                pos += right_size;
+
+                // Hole gets smaller
+                hole -= right_size;
+                moved[right] = true;
+
+                if hole == 0 {
+                    // Hole filled
+                    break;
+                }
+            }
+            // Hole not filled, keep searching
+            right -= 2;
+        }
+        // If hole still wasn't filled, then there are no more files <= the hole size
+        if hole != 0 {
+            update_cache(&mut cache, hole, 0);
+            pos += hole;
+        }
+        left += 1;
+    }
+
+    Ok(result.to_string())
+}
+// 6382582136592
+
+#[inline(always)]
+fn parse_u8(b: u8) -> usize {
+    (b - b'0') as usize
 }
 
 #[cfg(test)]
@@ -9,9 +91,21 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "2333133121414131402";
+        assert_eq!("2858", process(input)?);
         Ok(())
     }
 }
+/*
+
+00...111...2...333.44.5555.6666.777.888899
+0099.111...2...333.44.5555.6666.777.8888..
+0099.1117772...333.44.5555.6666.....8888..
+0099.111777244.333....5555.6666.....8888..
+00992111777.44.333....5555.6666.....8888..
+
+..992111777.44.333....5555.6666.....8888
+
+9*2+9*3+2*4+1*5+1*6+1*7+7*8+7*9+7*10+4*12+4*13+3*15+3*16+3*17+5*22+5*23+5*24+5*25 +6*27+6*28+6*29+6*30 + 8*36 + 8*37 + 8*38 + 8*39
+
+*/
